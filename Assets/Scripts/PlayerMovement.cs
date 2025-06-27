77765bbf50eff33193c3eef3fa139cfb79e04c29
Chinacommand-CM <motstudio@163.com>
2025-06-28 03:01:57 +0800

    CM250628学习：跳跃和潜行的定义和使用&更改部分unity数值

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,12 +13,29 @@ public class PlayerMovement : MonoBehaviour
 
     private float xVelocity;
 
-    [Header("状态")] public bool isCrouching ;
+    [Header("状态")] 
+    public bool isCrouching ;
+    public bool isGround;
+    public bool isJumping;
+    
     // 这里是碰撞体尺寸
     private Vector2 Standsize;
     private Vector2 Crouchsize;
     private Vector2 StandOffset;
     private Vector2 CrouchOffset;
+    [Header("跳跃参数")] 
+    public float jumpForce = 6.3f;
+    public float jumpHoldForce = 1.9f;
+    public float jumpHoldDuration = 0.1f;
+    public float crouchJumpBoost = 2.5f;
+
+    private float jumpTime;
+
+    [Header("环境监测")] public LayerMask groundLayer;
+    
+    private bool jumpPress;       //跳跃
+    private bool crouchHoldPress; //下蹲
+    
     // 厘米君的第一个C#捏
     // 喵
     // 2025.6.22 凌晨3:37还在打qq电话的cm和huohua二人留
@@ -35,25 +52,66 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        jumpPress = Input.GetKey(KeyCode.Space);
+        crouchHoldPress = Input.GetKey(KeyCode.LeftShift)|| Input.GetKey(KeyCode.S);
     }
     void FixedUpdate()
-    { 
-       GroundMovement(); 
+    {
+        PhysicsCheck();
+        GroundMovement(); 
+        Jump();
     }
 
+    void PhysicsCheck()
+    {
+        isGround = bc .IsTouchingLayers(groundLayer);
+    }
+
+    void Jump()
+    {
+        if (jumpPress && isGround && !isJumping)
+        {
+            if (isCrouching)
+            {
+                Standup();
+                rb.AddForce(Vector2.up * crouchJumpBoost, ForceMode2D.Impulse);
+            }
+
+            //打过大括号huohua说就不用分号了
+            isGround=false;//疑似无必要
+            isJumping=true;
+            Vector2 jumpForceVector2 = new Vector2(0, jumpForce);
+            rb.AddForce(jumpForceVector2, ForceMode2D.Impulse);//ForceMode2D.Impulse:刚体立即移动,给对象一个瞬时初始力
+            jumpTime=Time.time + jumpHoldDuration;
+        }
+        else if (isJumping)
+        {
+            if (jumpPress)
+            {
+                rb.AddForce(new Vector2(0, jumpHoldForce), ForceMode2D.Impulse);
+            }
+            if (Time.time > jumpTime)
+            {
+                isJumping = false;
+            }
+        }
+    }
     private void GroundMovement()
     // 移动逻辑
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (crouchHoldPress&& !isCrouching&& isGround)
         {
             // 执行亲爱的下蹲捏
             Crouch();
         }
-        else if (isCrouching)
+        else if (isCrouching&& !crouchHoldPress)
 
         {
             // 贼也站起来了是吧
+            Standup();
+        }
+        else if (isCrouching&& !isGround)
+        { 
             Standup();
         }
         xVelocity = Input.GetAxis("Horizontal") ;
